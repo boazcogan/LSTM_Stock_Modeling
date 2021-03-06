9fbb6fc7bbc417488a3074ee3d2bf19fac0d9ab2
boazcogan <boazcogan@yahoo.com>
2021-03-05 17:33:04 -0800

    added rescaling of the datasets

--- main.py
+++ main.py
@@ -7,51 +7,54 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
-    """
-    train_features, train_targets, test_features, test_targets = gd.get_aggregated_dataset("commodities", 5, 7, 0.9)
-    batch_size = 32
-    print('----- Linear -----')
-    linear = LinearHandler(100, "MSE", None, 0.01, batch_size)
-    linear.create_model(train_features.shape[1], 1)
-    linear_losses = linear.train(train_features, train_targets)
-    plt.figure(0)
-    plt.title("Linear Loss")
-    plt.ylabel("loss")
-    plt.yscale('log')
-    plt.xlabel("epoch")
-    plt.plot(linear_losses)
-    plt.show()
-    linear.test(test_features, test_targets)
-    # print(f"\nThe MSE for the Linear test set is: {linear.test(test_features, test_targets)}\n\n")
+    blocks = ['LSTM']
+    if 'linear' in blocks:
+        train_features, train_targets, test_features, test_targets = gd.get_aggregated_dataset("commodities", 5, 7, 0.9)
+        batch_size = 256
+        print('----- Linear -----')
+        linear = LinearHandler(100, "MSE", None, 0.01, batch_size)
+        linear.create_model(train_features.shape[1], 1)
+        linear_losses = linear.train(train_features, train_targets)
+        plt.figure(0)
+        plt.title("Linear Loss")
+        plt.ylabel("loss")
+        plt.yscale('log')
+        plt.xlabel("epoch")
+        plt.plot(linear_losses)
+        plt.show()
+        linear_MSE, predictions = linear.test(test_features, test_targets)
+        # print(f"\nThe MSE for the Linear test set is: {linear.test(test_features, test_targets)}\n\n")
 
-    print('\n\n\n----- MLP -----')
-    mlp = MLPHandler(100, "MSE", None, 0.01, batch_size)
-    # they never specify the number of hidden nodes
-    # page 6: "a 2-layer neural network can be used to incorporate non-linear effects."
-    # Looks like just 1 hidden + 1 output
-    mlp.create_model(train_features.shape[1], 15, 1)
-    mlp_losses = mlp.train(train_features, np.squeeze(train_targets))
-    plt.figure(0)
-    plt.title("MLP Loss")
-    plt.ylabel("loss")
-    plt.yscale('log')
-    plt.xlabel("epoch")
-    plt.plot(mlp_losses)
-    plt.show()
-    mlp.test(test_features, test_targets)
-    # print(f"\nThe MSE for the MLP test set is: {linear.test(test_features, np.squeeze(test_targets))}\n\n")
-    """
-    train_features, train_targets, test_features, test_targets = gd.get_normal_dataset("commodities", 0.9, 7, normalize=True)
+    if 'MLP' in blocks:
+        print('\n\n\n----- MLP -----')
+        mlp = MLPHandler(100, "MSE", None, 0.01, batch_size)
+        # they never specify the number of hidden nodes
+        # page 6: "a 2-layer neural network can be used to incorporate non-linear effects."
+        # Looks like just 1 hidden + 1 output
+        mlp.create_model(train_features.shape[1], 15, 1)
+        mlp_losses = mlp.train(train_features, np.squeeze(train_targets))
+        plt.figure(0)
+        plt.title("MLP Loss")
+        plt.ylabel("loss")
+        plt.yscale('log')
+        plt.xlabel("epoch")
+        plt.plot(mlp_losses)
+        plt.show()
+        MLP_MSE, predictions = mlp.test(test_features, np.squeeze(test_targets))
+        # print(f"\nThe MSE for the MLP test set is: {linear.test(test_features, np.squeeze(test_targets))}\n\n")
 
-    print('\n\n\n----- LSTM -----')
-    lstm = LSTMHandler(100, "MSE", None, 0.01, 0)
-    lstm.create_model(train_features.shape[1], 15, 1, 1)
-    lstm_losses = lstm.train(train_features, train_targets)
-    plt.figure(0)
-    plt.title("lstm Loss")
-    plt.ylabel("loss")
-    plt.yscale('log')
-    plt.xlabel("epoch")
-    plt.plot(lstm_losses)
-    plt.show()
-    # print(f"\nThe MSE for the LSTM test set is: {linear.test(test_features, test_targets)}\n\n")
+    if 'LSTM' in blocks:
+        train_features, train_targets, test_features, test_targets = gd.get_normal_dataset("commodities", 0.9, 7, normalize=True)
+        print('\n\n\n----- LSTM -----')
+        lstm = LSTMHandler(100, "MSE", None, 0.01, 0)
+        lstm.create_model(train_features.shape[1], 15, 1, 1)
+        lstm_losses = lstm.train(train_features, train_targets)
+        plt.figure(0)
+        plt.title("lstm Loss")
+        plt.ylabel("loss")
+        plt.yscale('log')
+        plt.xlabel("epoch")
+        plt.plot(lstm_losses)
+        plt.show()
+        # print(f"\nThe MSE for the LSTM test set is: {linear.test(test_features, test_targets)}\n\n")
+        lstm.test(test_features, test_targets)
