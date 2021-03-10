7cfea9fb8d9537b6843da1a2f909b81f366feba3
boazcogan <boazcogan@yahoo.com>
2021-03-10 13:18:36 -0800

    cleaning commented code

--- main.py
+++ main.py
@@ -62,19 +62,11 @@ if __name__ == '__main__':
         linear = LinearHandler(epochs, loss_function, None, 0.01, batch_size, l1enable=regularization)
         linear.create_model(train_features.shape[1], 1)
         linear_losses = linear.train(train_features, train_targets)
-        # plt.figure(0)
-        # plt.title("Linear Loss")
-        # plt.ylabel("loss")
-        # plt.yscale('log')
-        # plt.xlabel("epoch")
-        # plt.plot(linear_losses)
-        # plt.show()
         _predictions = []
         for i in range(test_features.shape[0]):
             _, pred = linear.test(test_features[i].astype(np.float32), test_targets[i].astype(np.float32))
             _predictions.append(pred.detach().numpy())
         linear_performance, linear_profitability = model_trading(test_targets, _predictions, lookahead=target_lookahead)
-        # print(f"\nThe MSE for the Linear test set is: {linear.test(test_features, test_targets)}\n\n")
 
     if 'MLP' in blocks:
         print('\n\n\n----- MLP -----')
@@ -84,19 +76,11 @@ if __name__ == '__main__':
         # Looks like just 1 hidden + 1 output
         mlp.create_model(train_features.shape[1], 15, 1)
         mlp_losses = mlp.train(train_features, np.squeeze(train_targets))
-        # plt.figure(0)
-        # plt.title("MLP Loss")
-        # plt.ylabel("loss")
-        # plt.yscale('log')
-        # plt.xlabel("epoch")
-        # plt.plot(mlp_losses)
-        # plt.show()
         _predictions = []
         for i in range(test_features.shape[0]):
             _, pred = mlp.test(test_features[i].astype(np.float32), np.squeeze(test_targets[i].astype(np.float32)))
             _predictions.append(pred.detach().numpy())
         mlp_performance, mlp_profitability = model_trading(test_targets, _predictions, lookahead=target_lookahead)
-        # print(f"\nThe MSE for the MLP test set is: {linear.test(test_features, np.squeeze(test_targets))}\n\n")
 
     if 'LSTM' in blocks:
         _predictions = []
@@ -104,29 +88,17 @@ if __name__ == '__main__':
                                                                                                 aggregate_days=1,
                                                                                                 target_lookahead=2)
         # aggregate the training set together, no need to differentiate between the different sets during training
-        # train_features = np.concatenate(train_features).astype(np.float32)
-        # train_targets = np.concatenate(train_targets).astype(np.float32)
         print('\n\n\n----- LSTM -----')
         lstm = LSTMHandler(epochs, loss_function, None, 0.01, batch_size, l1enable=regularization)
         lstm.create_model(train_features[0].shape[1], 15, 1, 1)
         lstm_losses = lstm.train(train_features, train_targets)
-        # plt.figure(0)
-        # plt.title("lstm Loss")
-        # plt.ylabel("loss")
-        # # plt.yscale('log')
-        # plt.xlabel("epoch")
-        # plt.plot(np.squeeze(lstm_losses))
-        # plt.show()
-        # print(f"\nThe MSE for the LSTM test set is: {linear.test(test_features, test_targets)}\n\n")
         for i in range(test_features.shape[0]):
             _, pred = lstm.test(test_features[i].astype(np.float32), test_targets[i].astype(np.float32))
             _predictions.append(pred)
-            # _predictions.append(pred.detach().numpy())
         lstm_performance, lstm_profitability = model_trading(test_targets, _predictions, lookahead=target_lookahead)
         best_possible_performance = model_trading(test_targets, test_targets, lookahead=2)
     max_size = max(lstm_performance.shape[0], linear_performance.shape[0], mlp_performance.shape[0])
     lstm_key = np.linspace(0, lstm_performance.shape[0], max_size)
-    # lstm_performance = np.interp(lstm_performance, lstm_key)
     lstm_performance = np.interp(lstm_key, np.arange(lstm_performance.shape[0]), lstm_performance)
     linear_key = np.linspace(0, linear_performance.shape[0], max_size)
     linear_performance = np.interp(linear_key, np.arange(linear_performance.shape[0]), linear_performance)
